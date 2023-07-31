85869eae89bed47a090832763351786467478daa
ShaikhShama <81861355+ShaikhShama@users.noreply.github.com>
2023-07-31 09:45:17 +0530

    Firstfile

---
+++ Fist.java
@@ -0,0 +1,30 @@
+package com.demo;
+
+public class Fist {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Edubridge India Pvt Ltd");
+		System.out.println("Java Full stack");
+		System.out.println("Batch: 9844 and 9845");
+		System.out.println("Time: 7am to 10am");
+		
+		byte demo1 = 5;
+		System.out.println("Byte Value : " +demo1);
+		
+		short demo2 = 50;
+		System.out.println("Short Value : " +demo2);
+		
+		
+		
+		int demo = 100;
+		demo = 200;
+		System.out.println("Demo value : " +demo);
+		
+		long demo4 = 3000;
+		System.out.println("Long Value : " +demo4);
+		
+
+	}
+
+}
